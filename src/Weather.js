33ded639c435f3e047d09f62b4ea276ecfdc7315
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2024-02-24 21:59:03 -0500

    Changed the audio to hotpink

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
       height="80"
       width="80"
       radius="9"
-      color="green"
+      color="hotpink"
       ariaLabel="loading"
       wrapperStyle
       wrapperClass
